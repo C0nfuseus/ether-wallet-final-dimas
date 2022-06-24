a2b1bbec4703722984d8c8ce25f7d643f0fcc731
Dimas Nazli <nazlibahaduri@gmail.com>
2022-06-24 19:24:52 +0700

    .

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ app.get("/about", (req,res) => {
     res.sendFile(path.join(__dirname + "/about.html"));
 })
 
-app.listen(process.env.)
-
 app.listen(process.env.PORT || 3000, function(){
     console.log('express server listening on port %d in %s mode', this.address().port, app.settings.env);
   });
