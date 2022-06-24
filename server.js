6f840ae66908082fff670baad480144211c35bae
Dimas Nazli <nazlibahaduri@gmail.com>
2022-06-24 19:24:00 +0700

    .

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.get("/about", (req,res) => {
     res.sendFile(path.join(__dirname + "/about.html"));
 })
 
-const server = app.listen(5000);
-const portNumber = server.address().port;
-console.log(`RUnning on port: ${portNumber}`);
+app.listen(process.env.)
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log('express server listening on port %d in %s mode', this.address().port, app.settings.env);
+  });
